2b8f161bf3976a2c6c0e3c0728b28192065bb549
Daniel <ge24cer@mytum.de>
2021-03-25 07:39:15 +0100

    finished the tictactoe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -18,6 +18,8 @@ free_spots = [1,2,3,4,5,6,7,8,9]
 starting_field = ["1","2","3","4","5","6","7","8","9"]
 X_spots = []
 O_spots = []
+player_spots = []
+bot_spots = []
 
 bot = None
 bot_player = None
@@ -90,17 +92,137 @@ while(len(free_spots) != 0 and win == 0 and bot == "n"):
 
 
 #runnig the game against a bot
-def random_decision(f_s):
-	return random.choice(f_s)
+def random_decision():
+	return random.choice(free_spots)
 
-def perfect_decision(f_s=free_spots, X_s=X_spots, O_s=O_spots, b_p=bot_player):
-	if 5 in f_s:
+def perfect_decision():
+	#print(starting_field)
+	if 5 in free_spots:
 		return(5)
-	elif
+	elif len(free_spots) == 1:
+		return(free_spots[0])
+	elif starting_field[5] != "5" and len(free_spots) == 8:
+		return(random.choice([1,3,7,9]))
 
-	
+	#deadlock preventions:
+	elif all(elem in player_spots for elem in [1,9]) and 4 in free_spots:
+		return(4)
+	elif all(elem in player_spots for elem in [3,7]) and 4 in free_spots:
+		return(4)
+
+	#all horizontal wins
+	elif all(elem in bot_spots for elem in [1,2]) and 3 in free_spots:
+		return(3)
+	elif all(elem in bot_spots for elem in [1,3]) and 2 in free_spots:
+		return(2)
+	elif all(elem in bot_spots for elem in [2,3]) and 1 in free_spots:
+		return(1)
+	elif all(elem in bot_spots for elem in [4,5]) and 6 in free_spots:
+		return(6)
+	elif all(elem in bot_spots for elem in [4,6]) and 5 in free_spots:
+		return(5)
+	elif all(elem in bot_spots for elem in [5,6]) and 4 in free_spots:
+		return(4)
+	elif all(elem in bot_spots for elem in [7,8]) and 9 in free_spots:
+		return(9)
+	elif all(elem in bot_spots for elem in [7,9]) and 8 in free_spots:
+		return(8)
+	elif all(elem in bot_spots for elem in [8,9]) and 7 in free_spots:
+		return(7)
+
+	#all vectical wins
+	elif all(elem in bot_spots for elem in [1,4]) and 7 in free_spots:
+		return(7)
+	elif all(elem in bot_spots for elem in [1,7]) and 4 in free_spots:
+		return(4)
+	elif all(elem in bot_spots for elem in [4,7]) and 1 in free_spots:
+		return(1)
+	elif all(elem in bot_spots for elem in [2,5]) and 8 in free_spots:
+		return(8)
+	elif all(elem in bot_spots for elem in [2,8]) and 5 in free_spots:
+		return(5)
+	elif all(elem in bot_spots for elem in [5,8]) and 2 in free_spots:
+		return(2)
+	elif all(elem in bot_spots for elem in [3,6]) and 9 in free_spots:
+		return(9)
+	elif all(elem in bot_spots for elem in [3,9]) and 6 in free_spots:
+		return(6)
+	elif all(elem in bot_spots for elem in [6,9]) and 3 in free_spots:
+		return(3)
+
+	#all diagonal wins
+	elif all(elem in bot_spots for elem in [1,5]) and 9 in free_spots:
+		return(9)
+	elif all(elem in bot_spots for elem in [1,9]) and 5 in free_spots:
+		return(5)
+	elif all(elem in bot_spots for elem in [5,9]) and 1 in free_spots:
+		return(1)
+	elif all(elem in bot_spots for elem in [3,5]) and 7 in free_spots:
+		return(7)
+	elif all(elem in bot_spots for elem in [3,7]) and 5 in free_spots:
+		return(5)
+	elif all(elem in bot_spots for elem in [5,7]) and 3 in free_spots:
+		return(3)
+
+	#all horizontal preventions:
+	elif all(elem in player_spots for elem in [1,2]) and 3 in free_spots:
+		return(3)
+	elif all(elem in player_spots for elem in [1,3]) and 2 in free_spots:
+		return(2)
+	elif all(elem in player_spots for elem in [2,3]) and 1 in free_spots:
+		return(1)
+	elif all(elem in player_spots for elem in [4,5]) and 6 in free_spots:
+		return(6)
+	elif all(elem in player_spots for elem in [4,6]) and 5 in free_spots:
+		return(5)
+	elif all(elem in player_spots for elem in [5,6]) and 4 in free_spots:
+		return(4)
+	elif all(elem in player_spots for elem in [7,8]) and 9 in free_spots:
+		return(9)
+	elif all(elem in player_spots for elem in [7,9]) and 8 in free_spots:
+		return(8)
+	elif all(elem in player_spots for elem in [8,9]) and 7 in free_spots:
+		return(7)
+
+	#all vectical preventions
+	elif all(elem in player_spots for elem in [1,4]) and 7 in free_spots:
+		return(7)
+	elif all(elem in player_spots for elem in [1,7]) and 4 in free_spots:
+		return(4)
+	elif all(elem in player_spots for elem in [4,7]) and 1 in free_spots:
+		return(1)
+	elif all(elem in player_spots for elem in [2,5]) and 8 in free_spots:
+		return(8)
+	elif all(elem in player_spots for elem in [2,8]) and 5 in free_spots:
+		return(5)
+	elif all(elem in player_spots for elem in [5,8]) and 2 in free_spots:
+		return(2)
+	elif all(elem in player_spots for elem in [3,6]) and 9 in free_spots:
+		return(9)
+	elif all(elem in player_spots for elem in [3,9]) and 6 in free_spots:
+		return(6)
+	elif all(elem in player_spots for elem in [6,9]) and 3 in free_spots:
+		return(3)
+
+	#all diagonal preventions
+	elif all(elem in player_spots for elem in [1,5]) and 9 in free_spots:
+		return(9)
+	elif all(elem in player_spots for elem in [1,9]) and 5 in free_spots:
+		return(5)
+	elif all(elem in player_spots for elem in [5,9]) and 1 in free_spots:
+		return(1)
+	elif all(elem in player_spots for elem in [3,5]) and 7 in free_spots:
+		return(7)
+	elif all(elem in player_spots for elem in [3,7]) and 5 in free_spots:
+		return(5)
+	elif all(elem in player_spots for elem in [5,7]) and 3 in free_spots:
+		return(3)
+
 	else:
-		return(f_s[0])
+		(print("should never happen"))
+		return(free_spots[0])
+
+
 
 if current_player == "X":
 	bot_turn = 1
@@ -112,8 +234,9 @@ while(len(free_spots) != 0 and win == 0 and bot == "y"):
 
 	if bot_turn == 0:
 		print("\nThe Bots turn:")
-		# new_value_bot = random_decision(free_spots)
-		new_value_bot=perfect_decision(free_spots)
+		# new_value_bot = random_decision()
+		new_value_bot=perfect_decision()
+		bot_spots+=[new_value_bot]
 		starting_field[new_value_bot-1] = bot_player
 		free_spots.remove(new_value_bot)
 
@@ -127,13 +250,14 @@ while(len(free_spots) != 0 and win == 0 and bot == "y"):
 			new_value = int(input("\n"+"Player "+current_player+" choose a field: "))
 
 		bot_turn -= 1
+		player_spots+=[new_value]
 		starting_field[new_value-1] = current_player
 		free_spots.remove(new_value)
 
 	winner, win = winning(starting_field)
 
 
-
+print(bot_spots, player_spots)
 
 
 
